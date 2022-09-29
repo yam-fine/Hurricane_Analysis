e6d4d450d6ed952dee7c81fc0a87e8fa84c3b7dd
rom954 <rom.maltser@mail.huji.ac.il>
2022-09-29 14:06:29 +0300

    PUSH

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,6 +7,15 @@ class PreProcess():
         for i, name in enumerate(self.file["name"]):
             if name == "Unnamed":
                 self.file["name"][i] = "Unnamed" + str(i)
+        pd.to_datetime(self.file["date"])
+        f = lambda x, y: x.replace(year=y)
+        self.file["date"] = self.file.apply(lambda x: f(x["date"], x["year"]), axis=1)
+        # for date, h_year, index in zip(self.file["date"], self.file["year"], range(0, len(self.file["date"].unique()))):
+        #     self.file["date"][index] = date.replace(year=h_year)
+
+
+
+
 
     def get_file(self):
         return self.file
