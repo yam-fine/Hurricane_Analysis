c3939eb126990afa1e519053efe0ba66d5b13ea9
rom954 <rom.maltser@mail.huji.ac.il>
2022-09-29 14:28:42 +0300

    pu

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 
+
+StocksSymbol = ["LOW","BJ","FND","ORLY","CVNA","ALL" ,"PGR","TRV","DHI","LEN"]
+ColsToInsert = ["Hurricane","Category","Date","Stock","7 change", "20 change","30 change","50 change"]
 
 class PreProcess():
     def __init__(self, path):
@@ -12,6 +16,15 @@ class PreProcess():
         self.file["date"] = self.file.apply(lambda x: f(x["date"], x["year"]), axis=1)
         # for date, h_year, index in zip(self.file["date"], self.file["year"], range(0, len(self.file["date"].unique()))):
         #     self.file["date"][index] = date.replace(year=h_year)
+        numOfStocks = len(ColsToInsert) ; numOfHurr = len(self.file["date"].unique())
+        finalPD = pd.DataFrame(np.zeros((numOfStocks *numOfHurr, numOfStocks)),columns=ColsToInsert)
+
+        index = 0
+        # for date in self.file["date"]:
+        #     for symbol in StocksSymbol:
+
+
+
 
 
 
