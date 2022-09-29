59c325519fb56e0c7ffdeffbcbcb44ffba1d3fb1
rom954 <rom.maltser@mail.huji.ac.il>
2022-09-29 15:14:26 +0300

    pu

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,11 +3,12 @@ import numpy as np
 
 
 StocksSymbol = ["LOW","BJ","FND","ORLY","CVNA","ALL" ,"PGR","TRV","DHI","LEN"]
-ColsToInsert = ["Hurricane","Category","Date","Stock","7 change", "20 change","30 change","50 change"]
+ColsToInsert = ["hurricane","category","date","stock","7change", "20change","30change","50change"]
 
 class PreProcess():
     def __init__(self, path):
         self.file = pd.ExcelFile(path).parse('Sheet1')
+        self.stockfin =.....
         for i, name in enumerate(self.file["name"]):
             if name == "Unnamed":
                 self.file["name"][i] = "Unnamed" + str(i)
@@ -20,9 +21,16 @@ class PreProcess():
         finalPD = pd.DataFrame(np.zeros((numOfStocks *numOfHurr, numOfStocks)),columns=ColsToInsert)
 
         index = 0
-        # for date in self.file["date"]:
-        #     for symbol in StocksSymbol:
+        for i, row in self.file.iterrows():
+            for symbol in StocksSymbol:
 
+                new_row = {"Hurricane": row["name"],"Category": row["category"],"Date":row["date"],"Stock":symbol,"7change":calcChange(row["date"],7,row["name"]), "20change" : calcChange(row["date"],20,row["name"]) ,"30change" : calcChange(row["date"],30,row["name"]),"50change": calcChange(row["date"],30,row["name"])}
+                finalPD = finalPD.append(new_row, ignore_index=True)
+
+    def calcChange(self, date, days, stock):
+        dateVal = self.stockfin[(self.stockfin["stock"] == stock) & (self.stockfin["Date"] == date)]["open"]
+        laterVal = self.stockfin[(self.stockfin["stock"] == stock) & (self.stockfin["Date"] == date + days)]["open"]
+        return ((laterVal-dateVal)/dateVal)*100
 
 
 
